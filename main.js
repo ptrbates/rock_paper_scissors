d34f1798049dc7de570923d0e8e55c5dd7105bc9
ptrbates <peterbates@protonmail.com>
2019-03-10 19:24:06 -0700

    add images to ui

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const computerPlay = () => {
   const randInt = Math.floor(Math.random() * 3);
   switch (randInt) {
     case 0:
-      return "Rock";
+      return "rock";
     case 1:
-      return "Paper";
+      return "paper";
     default:
-      return "Scissors";
+      return "scissors";
   }
 }
+
 const testComputerPlay = n => {
   let [rocks, papers, scissors] = [0, 0, 0];
   for (let i = 0; i < n; i++) {
@@ -19,38 +20,61 @@ const testComputerPlay = n => {
   }
   console.log(rocks, papers, scissors);
 }
+
 const playRound = (playerSelection, computerSelection) => {
-  let ps = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-  if (ps === computerSelection) {
-    return "It's a boring tie.";
-  } else if (ps === 'Rock' && computerSelection === 'Scissors' ||
-    ps === 'Scissors' && computerSelection === 'Paper' ||
-    ps === 'Paper' && computerSelection === 'Rock') {
+  if (playerSelection === computerSelection) {
+    return 0;
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+    playerSelection === 'scissors' && computerSelection === 'paper' ||
+    playerSelection === 'paper' && computerSelection === 'rock') {
     return 1;
   } else {
-    return `You lose. ${computerSelection} beats ${ps}.`;
+    return -1;
   }
 }
-const game = () => {
-  let playerWins = 0;
-  for (let i = 0; i < 5; i++) {
-    const playerChoice = prompt("Paper, Rock, or Scissors?");
-    const computerChoice = computerPlay();
-    const res = playRound(playerChoice, computerChoice);
 
-    if (res === 1) {
-      console.log(`You win. ${playerChoice} beats ${computerChoice}.`);
-      playerWins++;
-    } else if (res === -1) {
-      console.log(`You lose. ${computerChoice} beats ${playerChoice}.`);
-    } else {
-      console.log(`It's a boring tie of ${playerChoice}s.`);
-    }
-  }
-
-  if (playerWins >= 3) {
-    console.log(`Having won ${playerWins} rounds, you win.`);
+let computerWins = 0;
+let tiedGames = 0;
+let playerWins = 0;
+const scoreKeeper = result => {
+  if (result === -1) {
+    computerWins++;
+  } else if (result === 0) {
+    tiedGames++;
   } else {
-    console.log(`You only won ${playerWins} rounds. Better luck next time.`);
+    playerWins++;
   }
 }
+
+const updateScoreBoard = result => {
+  const pw = document.getElementById('playerWins');
+  const cw = document.getElementById('computerWins');
+  const tg = document.getElementById('tiedGames');
+  pw.textContent = `Player Wins: ${playerWins}`;
+  cw.textContent = `Computer Wins: ${computerWins}`;
+  tg.textContent = `Tied Games: ${tiedGames}`;
+  const announcer = document.getElementById('announcer');
+  if (playerWins >= 5) {
+    announcer.textContent = `Player wins the game!`;
+  } else if (computerWins >= 5) {
+    announcer.textContent = `Computer wins the game!`;
+  }
+}
+
+const updateComputerChooser = choice => {
+  const computerChooser = document.getElementById('computerChooser');
+  const initText = document.getElementById('initText');
+  computerChooser.src = `images/${choice}.jpg`;
+  initText.textContent = '';
+}
+
+const imgs = document.getElementsByClassName('playerChoices');
+for (i of imgs) {
+  i.addEventListener('click', e => {
+    const compChoice = computerPlay();
+    updateComputerChooser(compChoice);
+    const result = playRound(e.target.id, compChoice);
+    scoreKeeper(result);
+    updateScoreBoard(result);
+  });
+};
